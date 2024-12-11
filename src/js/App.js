565960add5c9318c83e2e4f2a0f83ef58c1497cd
shashankpm7 <moharirshashank@gmail.com>
2024-12-11 20:20:02 +0530

    Initial commit of portfolio website

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to My Portfolio</h1>
+            <button>Contact Me</button>
+        </div>
+    );
+}
+
+export default App;
